d1a9bad58b96209733f8512ba0dcbc753bfe6196
housuhong2017 <371926913@qq.com>
2018-09-22 16:46:12 +0800

    任务三提交

---
+++ work3/js/style.js
@@ -0,0 +1,50 @@
+		function changeStyle(name){
+			var bk =document.getElementById("content");
+			var change=bk.style.background="gray";
+			localStorage.setItem('as',change);
+			window.onload=function(){
+				document.getElementsByTagName('p').background=localStorage.getItem('as');
+			}
+		}
+		function changeStyle1(name){
+			var bk =document.getElementById("content");
+			var change=bk.style.background="mediumpurple";
+			localStorage.setItem('as',change);
+			window.onload=function(){
+				document.getElementsByTagName('p').background=localStorage.getItem('as');
+			}
+		}
+		function changeStyle2(name){
+			var bk =document.getElementById("content");
+			var change=bk.style.background="red";
+			localStorage.setItem('as',change);
+			window.onload=function(){
+				document.getElementsByTagName('p').background=localStorage.getItem('as');
+			}
+		}
+		function changeStyle3(name){
+			var bk =document.getElementById("content");
+			var change=bk.style.background="deepskyblue";
+			localStorage.setItem('as',change);
+			window.onload=function(){
+				document.getElementsByTagName('p').background=localStorage.getItem('as');
+			}
+		}
+		function changeStyle4(name){
+			var bk =document.getElementById("content");
+			var change=bk.style.background="coral";
+			localStorage.setItem('as',change);
+			window.onload=function(){
+				document.getElementsByTagName('p').background=localStorage.getItem('as');
+			}
+		}
+		function changeStyle5(name){
+			var bk =document.getElementById("content");
+			var change=bk.style.background="greenyellow";
+			localStorage.setItem('as',change);
+			window.onload=function(){
+				document.getElementsByTagName('p').background=localStorage.getItem('as');
+			}
+		}
+		
+
